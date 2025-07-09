e25d9c493a738063bb829715daa46e12ed57cf8c
Afran Anas <111559691+afrananas@users.noreply.github.com>
2025-07-09 10:50:41 +0600

    summery

--- Car_Workshop_Appointment_System/includes/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-    <footer class="main-footer">
-        <div class="container">
-            <p>&copy; <?= date('Y') ?> <?= sanitize_output(APP_NAME) ?>. All rights reserved.</p>
-        </div>
-    </footer>
-    <script src="<?= BASE_URL ?>/assets/js/main.js"></script>
-</body>
-</html>
